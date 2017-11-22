483516df9d3e183be80a468d036a2742aa6e55ed
zer0ring <christocratic@hotmail.com>
2017-11-22 23:36:26 +1000

    Update LTC.info_prefill_GM.user.js

--- LTC.info_prefill_GM.user.js
+++ LTC.info_prefill_GM.user.js
@@ -7,6 +7,8 @@
 // @copyright   2017, neuromancer
 // @homepageURL https://steemit.com/@neuromancer
 // @supportURL  https://openuserjs.org/scripts/neuromancer/LTC.info_address_prefill/issues
+// @updateURL   https://openuserjs.org/meta/neuromancer/LTC.info_address_prefill_GM.meta.js
+// @downloadURL https://openuserjs.org/install/neuromancer/LTC.info_address_prefill_GM.user.js
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @include     http://litecoinfaucet.info/*
 // @run-at      document-end
