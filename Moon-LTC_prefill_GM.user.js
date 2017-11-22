f55dd79eaa0c2fa80c56d41ea462cb19e7eddef4
zer0ring <christocratic@hotmail.com>
2017-11-23 00:20:25 +1000

    Update Moon-LTC_prefill_GM.user.js

--- Moon-LTC_prefill_GM.user.js
+++ Moon-LTC_prefill_GM.user.js
@@ -2,7 +2,7 @@
 // @name        Moon LTC prefill GM
 // @namespace   unoriginal-but-unique
 // @description Moon LTC address prefill code for Greasemonkey
-// @icon        http://moonliteco.in/img/btc.png
+// @icon        http://moonliteco.in/img/litecoin.png
 // @version     1
 // @copyright   2017, neuromancer
 // @homepageURL https://steemit.com/@neuromancer
