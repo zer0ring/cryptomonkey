07e31ec6e4c3177edf69be042964ece5b74658d6
zer0ring <christocratic@hotmail.com>
2017-11-22 23:43:28 +1000

    Create ETH.info_prefill_GM.user.js

---
+++ ETH.info_prefill_GM.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        ETH.info prefill
+// @namespace   unoriginal-but-unique
+// @description ETH.info address prefill code for Greasemonkey
+// @icon        http://ethereumfaucet.info/view/images/favicon.png
+// @version     1
+// @copyright   2017, neuromancer
+// @homepageURL https://steemit.com/@neuromancer
+// @supportURL  https://openuserjs.org/scripts/neuromancer/ETH.info_address_prefill/issues
+// @updateURL   https://openuserjs.org/meta/neuromancer/ETH.info_address_prefill_GM.meta.js
+// @downloadURL https://openuserjs.org/install/neuromancer/ETH.info_address_prefill_GM.user.js
+// @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
+// @include     http://ethereumfaucet.info/*
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+document.getElementById("address").value = "neuromancer";
