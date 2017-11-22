76f2342172257c9e0cbf4f99af697a810b9f29c3
zer0ring <christocratic@hotmail.com>
2017-11-22 23:38:41 +1000

    Update DCR.info_prefill_GM.user.js

--- DCR.info_prefill_GM.user.js
+++ DCR.info_prefill_GM.user.js
@@ -4,7 +4,11 @@
 // @description DCR.info address prefill code for Greasemonkey
 // @icon        http://decredfaucet.info/view/images/favicon.png
 // @version     1
-// @copyright   2017, neuromancer (https://steemit.com/@neuromancer)
+// @copyright   2017, neuromancer
+// @homepageURL https://steemit.com/@neuromancer
+// @supportURL  https://openuserjs.org/scripts/neuromancer/DCR.info_address_prefill/issues
+// @updateURL   https://openuserjs.org/meta/neuromancer/DCR.info_address_prefill_GM.meta.js
+// @downloadURL https://openuserjs.org/install/neuromancer/DCR.info_address_prefill_GM.user.js
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @include     http://decredfaucet.info/*
 // @run-at      document-end
