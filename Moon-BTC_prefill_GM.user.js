e429664bfb0d6022de20d8aae3034ffa6c4fb835
zer0ring <christocratic@hotmail.com>
2017-11-23 00:04:35 +1000

    Create Moon-BTC_prefill_GM.user.js

---
+++ Moon-BTC_prefill_GM.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Moon BTC prefill GM
+// @namespace   unoriginal-but-unique
+// @description Moon BTC address prefill code for Greasemonkey
+// @icon        http://moonbit.co.in/favicon.png
+// @version     1
+// @copyright   2017, neuromancer
+// @homepageURL https://steemit.com/@neuromancer
+// @supportURL  https://openuserjs.org/scripts/neuromancer/Moon-BTC_address_prefill/issues
+// @updateURL   https://openuserjs.org/meta/neuromancer/Moon-BTC_address_prefill_GM.meta.js
+// @downloadURL https://openuserjs.org/install/neuromancer/Moon-BTC_address_prefill_GM.user.js
+// @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
+// @include     http://moonbit.co.in/*
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+document.getElementById("BodyPlaceholder_PaymentAddressTextbox").value = "neuromancer";
