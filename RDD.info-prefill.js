4f4176e234e075fa6094ca36911019b35aea95d6
zer0ring <christocratic@hotmail.com>
2017-11-22 14:37:12 +1000

    Update RDD.info-prefill.js

--- RDD.info-prefill.js
+++ RDD.info-prefill.js
@@ -3,12 +3,11 @@
 // @description RDD.info faucet address prefill code for Greasemonkey
 // @icon        http://reddcoinfaucet.info/view/images/favicon.png
 // @namespace   unoriginal-but-unique
-// @include     http://reddcoinfaucet.info//*
+// @include     http://reddcoinfaucet.info/*
 // @version     1
-// @author      neuromancer
+// @copyright   2017, neuromancer https://steemit.com/@neuromancer
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @updateURL   https://openuserjs.org/meta/neuromancer/RDD.info-prefill-GM.meta.js
-// @downloadURL https://openuserjs.org/src/scripts/neuromancer/RDD.info-prefill-GM.js
 // @grant       none
 // ==/UserScript==
 document.getElementById("address").value = "neuromancer";
