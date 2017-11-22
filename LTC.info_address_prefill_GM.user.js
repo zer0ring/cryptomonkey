d892799ce0bb74aef5007cd9aaac9718552a716b
zer0ring <christocratic@hotmail.com>
2017-11-22 23:20:53 +1000

    Update LTC.info_address_prefill_GM.user.js

--- LTC.info_address_prefill_GM.user.js
+++ LTC.info_address_prefill_GM.user.js
@@ -4,7 +4,9 @@
 // @description LTC.info address prefill code for Greasemonkey
 // @icon        http://litecoinfaucet.info/view/images/favicon.png
 // @version     1
-// @copyright   2017, neuromancer (https://steemit.com/@neuromancer)
+// @copyright   2017, neuromancer
+// @homepageURL https://steemit.com/@neuromancer
+// @supportURL  https://openuserjs.org/scripts/neuromancer/LTC.info_address_prefill/issues
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @include     http://litecoinfaucet.info/*
 // @run-at      document-end
