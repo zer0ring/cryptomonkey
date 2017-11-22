575a6398eda616756927da753a70e17c661fadbe
zer0ring <christocratic@hotmail.com>
2017-11-22 14:41:09 +1000

    Update RDD.info-prefill.js

--- RDD.info-prefill.js
+++ RDD.info-prefill.js
@@ -5,7 +5,7 @@
 // @namespace   unoriginal-but-unique
 // @include     http://reddcoinfaucet.info/*
 // @version     1
-// @copyright   2017, neuromancer https://steemit.com/@neuromancer
+// @copyright   2017, neuromancer (https://steemit.com/@neuromancer)
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @updateURL   https://openuserjs.org/meta/neuromancer/RDD.info-prefill-GM.meta.js
 // @grant       none
