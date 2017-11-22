8974b2ea4c221484a68b81aab9e453ad41e1b174
zer0ring <christocratic@hotmail.com>
2017-11-23 00:06:50 +1000

    Create Moon-LTC_prefill_GM.user.js

---
+++ Moon-LTC_prefill_GM.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Moon LTC prefill GM
+// @namespace   unoriginal-but-unique
+// @description Moon LTC address prefill code for Greasemonkey
+// @icon        http://moonliteco.in/favicon.png
+// @version     1
+// @copyright   2017, neuromancer
+// @homepageURL https://steemit.com/@neuromancer
+// @supportURL  https://openuserjs.org/scripts/neuromancer/Moon-LTC_address_prefill/issues
+// @updateURL   https://openuserjs.org/meta/neuromancer/Moon-LTC_address_prefill_GM.meta.js
+// @downloadURL https://openuserjs.org/install/neuromancer/Moon-LTC_address_prefill_GM.user.js
+// @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
+// @include     http://moonliteco.in/*
+// @run-at      document-end
+// @grant       none
+// ==/UserScript==
+document.getElementById("BodyPlaceholder_PaymentAddressTextbox").value = "neuromancer";
