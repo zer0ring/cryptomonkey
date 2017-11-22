72e8636d0420f3da6c43fb781906b67d5c6f0e66
zer0ring <christocratic@hotmail.com>
2017-11-22 10:30:32 +1000

    Create  LTC.info-prefill.user.js
    litecoinfaucet.info coin address prefill userscript for Greasemonkey, replace neuromancer with your coin address

---
+++ LTC.info-prefill.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        LTC.info faucet address prefill
+// @namespace   unoriginal-but-unique
+// @include     http://litecoinfaucet.info/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+document.getElementById("address").value = "neuromancer";
