024e3fb186c818fad782f4a63cb9bc790656acc0
zer0ring <christocratic@hotmail.com>
2018-02-17 22:23:41 +1000

    Update LTC.info_prefill_GM.user.js
    Changed target URL proto to https

--- LTC.info_prefill_GM.user.js
+++ LTC.info_prefill_GM.user.js
@@ -3,14 +3,14 @@
 // @namespace   unoriginal-but-unique
 // @description LTC.info address prefill code for Greasemonkey
 // @icon        http://litecoinfaucet.info/view/images/favicon.png
-// @version     1
+// @version     1.1
 // @copyright   2017, neuromancer
 // @homepageURL https://steemit.com/@neuromancer
 // @supportURL  https://openuserjs.org/scripts/neuromancer/LTC.info_address_prefill/issues
 // @updateURL   https://openuserjs.org/meta/neuromancer/LTC.info_address_prefill_GM.meta.js
 // @downloadURL https://openuserjs.org/install/neuromancer/LTC.info_address_prefill_GM.user.js
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
-// @include     http://litecoinfaucet.info/*
+// @include     https://litecoinfaucet.info/*
 // @run-at      document-end
 // @grant       none
 // ==/UserScript==
