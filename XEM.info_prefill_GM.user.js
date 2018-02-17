3af50397919129689afef116309677e68c0f2fa9
zer0ring <christocratic@hotmail.com>
2018-02-17 22:26:02 +1000

    Update XEM.info_prefill_GM.user.js
    Changed target URL proto to https

--- XEM.info_prefill_GM.user.js
+++ XEM.info_prefill_GM.user.js
@@ -3,14 +3,14 @@
 // @namespace   unoriginal-but-unique
 // @description ETH.info address prefill code for Greasemonkey
 // @icon        http://nemfaucet.info/view/images/favicon.png
-// @version     1
+// @version     1.1
 // @copyright   2018, neuromancer
 // @homepageURL https://steemit.com/@neuromancer
 // @supportURL  https://openuserjs.org/scripts/neuromancer/XEM.info_address_prefill/issues
 // @updateURL   https://openuserjs.org/meta/neuromancer/XEM.info_address_prefill_GM.meta.js
 // @downloadURL https://openuserjs.org/install/neuromancer/XEM.info_address_prefill_GM.user.js
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
-// @include     http://nemfaucet.info/*
+// @include     https://nemfaucet.info/*
 // @run-at      document-end
 // @grant       none
 // ==/UserScript==
