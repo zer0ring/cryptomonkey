47d6a18c5874dc19017f6e8b57b08d29362b0d3c
zer0ring <christocratic@hotmail.com>
2018-02-17 22:26:55 +1000

    Update XRP.info_prefill_GM.user.js
    Changed target URL proto to https

--- XRP.info_prefill_GM.user.js
+++ XRP.info_prefill_GM.user.js
@@ -3,14 +3,14 @@
 // @namespace   unoriginal-but-unique
 // @description XRP.info address prefill code for Greasemonkey
 // @icon        http://xrpfaucet.info/view/images/favicon.png
-// @version     1
+// @version     1.1
 // @copyright   2017, neuromancer
 // @homepageURL https://steemit.com/@neuromancer
 // @supportURL  https://openuserjs.org/scripts/neuromancer/XRP.info_address_prefill/issues
 // @updateURL   https://openuserjs.org/meta/neuromancer/XRP.info_address_prefill_GM.meta.js
 // @downloadURL https://openuserjs.org/install/neuromancer/XRP.info_address_prefill_GM.user.js
 // @license     GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
-// @include     http://xrpfaucet.info/*
+// @include     https://xrpfaucet.info/*
 // @run-at      document-end
 // @grant       none
 // ==/UserScript==
